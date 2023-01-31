d0d3713dfc01cc6fb89fca3baa665b37225a4cc5
Garrett Krohn <gkrohn@kipsu.edu>
2023-01-31 15:14:18 -0600

    added a controller and got a 200 response

---
+++ server/src/Controller/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Controller extends AbstractController
+{
+    #[Route('/')]
+    #[Route('/health')]
+    public function health(): Response {
+        return new Response();
+    }
+}
