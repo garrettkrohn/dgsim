bb6245534014a6a5f86338a308f499f2cd90c278
Garrett Krohn <gkrohn@kipsu.edu>
2023-02-07 16:11:31 -0600

    generic add player rest endpoint successful

--- server/src/Controller/PlayerController.php
+++ server/src/Controller/PlayerController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Player;
 use App\Repository\ArchetypeRepository;
 use App\Repository\PlayerRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -32,9 +33,26 @@ class PlayerController extends AbstractController
     }
 
     #[Route('api/players', methods: ('POST'))]
-    public function createNewPlayer(Request $request): Response
+    public function createNewPlayer(Request $request, ArchetypeRepository $archetypeRepository, EntityManagerInterface $entityManager): Response
     {
-        $receivedNewPlayer = json_decode($request->getContent(), true);
-        return new JsonResponse($receivedNewPlayer);
+        $request = json_decode($request->getContent(), true);
+        $newPlayer = new Player();
+        $arch = $archetypeRepository->findOneBy(array('id' => 1));
+        $newPlayer->setArchetype($arch);
+        $newPlayer->setFirstName('Ricky');
+        $newPlayer->setLastName('Wysocki');
+        $newPlayer->setPuttSkill(100);
+        $newPlayer->setThrowPowerSkill(100);
+        $newPlayer->setThrowAccuracySkill(100);
+        $newPlayer->setScrambleSkill(100);
+        $newPlayer->setStartSeason(1);
+        $newPlayer->setIsActive(true);
+        $newPlayer->setBankedSkillPoints(0);
+
+        $entityManager->persist($newPlayer);
+        $entityManager->flush();
+
+        $response = new Response();
+        return $response->setStatusCode(Response::HTTP_ACCEPTED);
     }
 }
