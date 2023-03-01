05fb2e013ba32958d4e4fd1695011564a3e2ba18
Garrett Krohn <gkrohn@kipsu.edu>
2023-03-01 13:51:23 -0600

    cleaned up the grid

--- app/src/components/homepage/UpdateInput.tsx
+++ app/src/components/homepage/UpdateInput.tsx
@@ -3,14 +3,14 @@ import { ArrowRightIcon, ArrowLeftIcon } from '@heroicons/react/20/solid';
 
 const UpdateInput = () => {
   const arrowStyling = 'w-100 text-dgprimary';
-  const arrowBoxStyling = 'h-20 w-20 rounded-lg bg-dgsoftwhite';
+  const arrowBoxStyling = 'h-5/6 w-5/6 rounded-lg bg-dgsoftwhite';
   return (
-    <div className="col-start-2 col-end-5 m-auto grid grid-cols-3">
+    <div className="col-start-2 col-end-5 m-auto grid grid-cols-3 items-center justify-items-center">
       <div className={arrowBoxStyling}>
         <ArrowLeftIcon className={arrowStyling} />
       </div>
       <input
-        className="h-20 rounded-lg bg-dgsecondary text-center text-dgsoftwhite"
+        className="h-20 w-full rounded-lg bg-dgsecondary text-center text-dgsoftwhite"
         value="10"
       />
       <div className={arrowBoxStyling}>
