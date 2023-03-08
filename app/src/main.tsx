17b66b753020c2c6602a5e5729719cde2b2ebde7
Garrett Krohn <gkrohn@kipsu.edu>
2023-03-08 15:19:33 -0600

    added the auth provider

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-mychhcrwjquf7khu.us.auth0.com"
+      clientId="Xv6FqcDFJabqt80isNucoaRO3cIuMC9h"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+    ,
   </React.StrictMode>,
-)
+);
