c93245f8f8d8b9c2f65a62047c15ac8835ade2f8
Garrett Krohn <gkrohn@kipsu.edu>
2023-03-10 09:29:25 -0600

    validator works with createCourseDto as well as the array of CreateHoleDto

--- server/src/Controller/CourseController.php
+++ server/src/Controller/CourseController.php
@@ -2,30 +2,36 @@
 
 namespace App\Controller;
 
+use App\Dto\Incoming\CreateCourseDto;
 use App\Serialization\JsonSerializer;
+use App\Serialization\SerializationService;
 use App\Service\CourseService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CourseController extends AbstractController
+class CourseController extends ApiController
 {
     private CourseService $courseService;
+    private SerializationService $serializationService;
 
     /**
      * @param CourseService $courseService
      */
-    public function __construct(CourseService $courseService)
+    public function __construct(CourseService $courseService, SerializationService $serializationService)
     {
+        parent::__construct($serializationService);
         $this->courseService = $courseService;
     }
 
     #[Route('/api/courses', methods: ['POST'])]
-    public function createCourse():Response
+    public function createCourse(Request $request):Response
     {
-        $course = $this->courseService->createNewCourse();
-        return new JsonResponse($course);
+        $dto = $this->getValidatedDto($request, CreateCourseDto::class);
+//        $course = $this->courseService->createNewCourse();
+        return new JsonResponse('$course');
     }
 
     #[Route('api/courses', methods: ['GET'])]
