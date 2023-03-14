5f4f52ac06c008edc5b7b503e1c09a6e15aef3d6
Garrett Krohn <gkrohn@kipsu.edu>
2023-03-14 14:43:25 -0500

    added create a player route

---
+++ app/src/components/CreatePlayer/CreatePlayer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreatePlayer = () => {
+  return (
+    <div className="text-dgsoftwhite">
+      <div>Create a Player</div>
+    </div>
+  );
+};
+
+export default CreatePlayer;
