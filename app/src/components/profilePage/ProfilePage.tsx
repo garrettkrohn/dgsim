de8afd1d6e2ad98df49cf8ae2615cefca5adcbda
Garrett Krohn <gkrohn@kipsu.edu>
2023-04-11 16:44:11 -0500

    profile page color picker added

--- app/src/components/profilePage/ProfilePage.tsx
+++ app/src/components/profilePage/ProfilePage.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+// @ts-ignore
+import { SliderPicker } from 'react-color';
 
 const ProfilePage = () => {
-  return <div>Profile Page</div>;
+  const [backgroundColor, setBackgroundColor] = useState('#FF0000');
+  const [foregroundColor, setForegroundColor] = useState('#FF0000');
+
+  const handleBackgroundChangeComplete = (event: any) => {
+    setBackgroundColor(event.hex);
+  };
+
+  const handleForegroundChangeComplete = (event: any) => {
+    setForegroundColor(event.hex);
+  };
+
+  return (
+    <div className="flex justify-center text-dgsoftwhite">
+      <div className="w-80 py-4">
+        <div className="py-4">
+          <div>Select Background Color:</div>
+          <SliderPicker
+            color={backgroundColor}
+            onChangeComplete={handleBackgroundChangeComplete}
+          />
+        </div>
+        <div className="py-4">
+          <div>Select Foreground Color:</div>
+          <SliderPicker
+            color={foregroundColor}
+            onChangeComplete={handleForegroundChangeComplete}
+          />
+        </div>
+        <div className="flex flex-col">
+          <div>Preview</div>
+          <div
+            style={{
+              backgroundColor: backgroundColor,
+              width: 250,
+              height: 250,
+            }}
+          >
+            Background
+          </div>
+          <div
+            style={{
+              backgroundColor: foregroundColor,
+              width: 250,
+              height: 250,
+            }}
+          >
+            Foreground
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
